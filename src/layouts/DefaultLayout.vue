5ae2d43ab189ec2e10e31b2104169095e9c23fe2
HuyNQ <huynq@HuyNQ-FADERLESS-MAC.local>
2023-02-28 09:14:23 +0700

    update source base

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -20,6 +20,7 @@ import HeadNavbar from "../components/Nav/HeadNavbar.vue";
   grid-template-columns: 265px auto;
   column-gap: 2rem;
 }
+
 .main-content {
   background-color: #fff;
   border-radius: 0.75rem;
